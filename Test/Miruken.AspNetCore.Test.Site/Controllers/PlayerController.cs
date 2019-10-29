3645f88f0a21245a2b4973345f875d7f4a97b558
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2019-10-29 06:39:39 -0500

    Added Miruken.AspNetCore.Test.Site

---
+++ Test/Miruken.AspNetCore.Test.Site/Controllers/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿namespace Miruken.AspNetCore.Test.Site.Controllers
+{
+    using AspNetCore;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
+
+    public class Player 
+    {
+        public int    Id   { get; set; }
+        public string Name { get; set; }
+    }
+
+    [ApiController,
+     Route("[controller]")]
+    public class PlayerController : ContextualControllerBase
+    {
+        private readonly ILogger<PlayerController> _logger;
+
+        public PlayerController(ILogger<PlayerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet, Route("{id}")]
+        public Player GetPlayer(int id)
+        {
+            _logger.LogInformation("Getting player {0}", id);
+            return new Player { Id = id, Name = "Christiano Ronaldo" };
+        }
+
+        [HttpPost, Route("")]
+        public Player CreatePlayer(Player player)
+        {
+            _logger.LogInformation("Creating player");
+            player.Id = 1;
+            _logger.LogInformation("Created player {0}", JsonConvert.SerializeObject(player));
+            _logger.LogInformation("Created player", JsonConvert.SerializeObject(player));
+            _logger.LogInformation("Created player", player);
+            return player;        
+        }
+
+        [HttpPut, Route("")]
+        public Player UpdatePlayer(Player player)
+        {
+            return player;
+        }
+
+        [HttpPatch, Route("")]
+        public Player PatchPlayer(Player player)
+        {
+            return player;
+        }
+
+        [HttpDelete, Route("{id}")]
+        public void DeletePlayer(int id)
+        {
+        }
+    }
+}
---
+++ Test/Miruken.AspNetCore.Test.Site/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace Miruken.AspNetCore.Test.Site
+{
+    using System;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Register;
+    using Validate;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            return services.AddMiruken(configure => configure
+                .PublicSources(sources => sources.FromAssemblyOf<Startup>())
+                .WithAspNet(options => options.AddControllers())
+                .WithValidation()
+            );
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
