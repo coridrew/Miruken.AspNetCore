bc30449019db34ba51b1cb2775639fddb4caac80
Michael Dudley <michael.dudley@improving.com>
2019-11-22 10:27:50 -0600

    starting specific test projects for core 22 and 30

---
+++ Test/Miruken.AspNetCore.Test.Site3_0/Controllers/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿namespace Miruken.AspNetCore.Test.Site.Controllers
+{
+    using System.Threading.Tasks;
+    using Api;
+    using AspNetCore;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
+    using Tests;
+
+    [ApiController,
+     Route("[controller]")]
+    public class PlayerController : ContextualControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public PlayerController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet, Route("{id}")]
+        public async Task<Player> GetPlayer(int id)
+        {
+            _logger.LogInformation("Getting player {0}", id);
+            return (await Context.Send(new GetPlayer {PlayerId = id})).Player;
+        }
+
+        [HttpPost, Route("")]
+        public async Task<Player> CreatePlayer(Player player)
+        {
+            _logger.LogInformation("Creating player");
+            var created = await Context.Send(new CreatePlayer {Player = player});
+            _logger.LogInformation("Created player {Player}",
+                JsonConvert.SerializeObject(created.Player));
+            return player;        
+        }
+
+        [HttpPut, Route("")]
+        public async Task<Player> UpdatePlayer(Player player)
+        {
+            _logger.LogInformation("Updating player");
+            var updated = await Context.Send(new UpdatePlayer { Player = player });
+            _logger.LogInformation("Updated player {Player}",
+                JsonConvert.SerializeObject(updated.Player));
+            return player;
+        }
+
+        [HttpPatch, Route("")]
+        public Player PatchPlayer(Player player)
+        {
+            return player;
+        }
+
+        [HttpDelete, Route("{id}")]
+        public void DeletePlayer(int id)
+        {
+        }
+    }
+}
---
+++ Test/Miruken.AspNetCore.Test.Site3_0/Startup.cs
@@ -0,0 +1,57 @@
+﻿namespace Miruken.AspNetCore.Test.Site
+{
+    using System;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Register;
+    using Swagger;
+    using Swashbuckle.AspNetCore.Swagger;
+    using Tests;
+    using Validate;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(config =>
+            {
+                config.Filters.Add(typeof(TestApiExceptionFilter));
+            })
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version     = "v1",
+                    Title       = "Test Api",
+                    Description = "Swagger Integration with Miruken"
+                });
+                c.AddMiruken();
+            });
+
+            return services.AddMiruken(configure => configure
+                .PublicSources(sources => sources.FromAssemblyOf<PlayerHandler>())
+                .WithAspNet(options => options.AddControllers())
+                .WithValidation()
+            ).Build();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc()
+                .UseSwagger()
+                .UseSwaggerUI(c =>
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test Api"));
+        }
+    }
+}
